0fc26b672380d33419571d678eb7fc04840d1df2
Piotr Dobrzyński <piotr.dobrzynski@gmail.com>
2020-02-03 19:40:42 +0100

    udoskonalony to hex i to bin

--- src/com/company/NumberConverter.java
+++ src/com/company/NumberConverter.java
@@ -1,7 +1,7 @@
 package com.company;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+
 
 class NumberConverter {
         private static int number;
@@ -28,61 +28,55 @@ class NumberConverter {
 
          for (; number > 0; number=number / 2) {
              if (number % 2 == 0) {
-                 binaryArray.add(0, "0");
+                 sb.insert(0, "0");
              } else if (number % 2 != 0) {
-                 binaryArray.add(0, "1");
+                 sb.insert(0, "1");
              }
          }
 
-        for(String s: binaryArray) {
-            sb.append(s);
-        }
          System.out.println(sb.toString());
     }
 
     public static void toHex2 (int number) {
-        ArrayList<String> hexArray=new ArrayList<>();
         StringBuffer sb2=new StringBuffer();
 
         for (; number > 0; number/=16) {
             if (number % 16 == 0) {
-                hexArray.add(0, "0");
+               sb2.insert(0, "0");
             } else if (number % 16 == 1) {
-                hexArray.add(0, "1");
+                sb2.insert(0, "1");
             } else if (number % 16 == 2) {
-                hexArray.add(0, "2");
+                sb2.insert(0, "2");
             } else if (number % 16 == 3) {
-                hexArray.add(0, "3");
+                sb2.insert(0, "3");
             } else if (number % 16 == 4) {
-                hexArray.add(0, "4");
+                sb2.insert(0, "4");
             } else if (number % 16 == 5) {
-                hexArray.add(0, "5");
+                sb2.insert(0, "5");
             } else if (number % 16 == 6) {
-                hexArray.add(0, "6");
+                sb2.insert(0, "6");
             } else if (number % 16 == 7) {
-                hexArray.add(0, "7");
+                sb2.insert(0, "7");
             } else if (number % 16 == 8) {
-                hexArray.add(0, "8");
+                sb2.insert(0, "8");
             } else if (number % 16 == 9) {
-                hexArray.add(0, "9");
+                sb2.insert(0, "9");
             } else if (number % 16 == 10) {
-                hexArray.add(0, "a");
+                sb2.insert(0, "a");
             } else if (number % 16 == 11) {
-                hexArray.add(0, "b");
+                sb2.insert(0, "b");
             } else if (number % 16 == 12) {
-                hexArray.add(0, "c");
+                sb2.insert(0, "c");
             } else if (number % 16 == 13) {
-                hexArray.add(0, "d");
+                sb2.insert(0, "d");
             } else if (number % 16 == 14) {
-                hexArray.add(0, "e");
+                sb2.insert(0, "e");
             } else if (number % 16 == 15) {
-                hexArray.add(0, "f");
+                sb2.insert(0, "f");
             }
         }
-        for(String s: hexArray) {
-            sb2.append(s);
-        }
-        System.out.println(sb2.toString());
+
+         System.out.println(sb2.toString());
     }
 }
 
